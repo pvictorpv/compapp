92456748070ce4f8846d8510285d018d830d4f88
pvictorpv <paulovictor_net@hotmail.com>
2025-11-19 09:55:13 -0300

    Preparando backend para deploy

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
